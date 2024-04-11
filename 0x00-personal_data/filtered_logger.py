2d51d0ed292d72d0515afdc08c7b9d2ac4c61c9a
keme-ebi <kelvin6bolou@gmail.com>
2024-04-12 00:44:51 +0100

    added main function

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -69,3 +69,21 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     conn = mysql.connector.connect(user=user, password=pswd,
                                    host=host, database=db)
     return conn
+
+
+def main():
+    """main function"""
+    db = get_db()
+    logger = get_logger()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users;")
+    fields = cursor.column_names
+    for row in cursor:
+        message = "".join("{}={}; ".format(k, v) for k, v in zip(fields, row))
+        logger.info(message.strip())
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()

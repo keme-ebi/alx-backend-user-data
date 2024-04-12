6a4adb4153f5eb2a9ee319b26d12f754385973a5
keme-ebi <kelvin6bolou@gmail.com>
2024-04-12 01:11:36 +0100

    implemented a function to validate a password with the hashed password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -11,3 +11,12 @@ def hash_password(password: str) -> bytes:
         salted, hashed password
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """validates that the provided password matches the hashed password
+    Arg:
+        hashed_password(bytes): the hashed password
+        password: the password to check with
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)

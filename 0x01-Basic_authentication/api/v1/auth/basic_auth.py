de3adcf72347703cef0f785d5737d650304cde07
keme-ebi <kelvin6bolou@gmail.com>
2024-04-16 00:01:34 +0100

    added a method to BasicAuth that returns the value after Basic for now

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,4 +5,15 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """inherits from the Auth class"""
-    pass
+    def extract_base64_authorization_header(
+            self,
+            authorization_header: str
+    ) -> str:
+        """returns the Base64 part of the Authorization header"""
+        starts = "Basic "
+        if not authorization_header or not type(authorization_header) == str:
+            return None
+        if not authorization_header.startswith(starts):
+            return None
+        value = authorization_header[len(starts):]
+        return value

3fc8b4e39ae50f877ffb5913936aa3da65dfe57d
keme-ebi <kelvin6bolou@gmail.com>
2024-04-16 01:15:08 +0100

    added a method that returns a decoded value of a Base64 string

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """basic_auth module"""
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -9,7 +10,10 @@ class BasicAuth(Auth):
             self,
             authorization_header: str
     ) -> str:
-        """returns the Base64 part of the Authorization header"""
+        """returns the Base64 part of the Authorization header
+        Arg:
+            authorization_header: authorization header string
+        """
         starts = "Basic "
         if not authorization_header or not type(authorization_header) == str:
             return None
@@ -17,3 +21,22 @@ class BasicAuth(Auth):
             return None
         value = authorization_header[len(starts):]
         return value
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str
+    ) -> str:
+        """returns the decoded value of a Base64 string
+        Arg:
+            base64_authorization_header: string to decode
+        """
+        if not base64_authorization_header:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded = base64.b64decode(base64_authorization_header)
+            dec = decoded.decode()
+            return dec
+        except Exception:
+            return None

5c1e6b3351967c6f6583100f8b010775d0600112
keme-ebi <kelvin6bolou@gmail.com>
2024-04-18 00:25:08 +0100

    updated the @app.before_request method by adding an extra path in the list of excluded paths and checking if both auth.authorization_header(request) and auth.session_cookie(request) return None, and the aborts with 401

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -56,10 +56,12 @@ def before_request():
     excluded_path = [
             "/api/v1/status/",
             "/api/v1/unauthorized/",
-            "/api/v1/forbidden"
+            "/api/v1/forbidden",
+            "/api/v1/auth_session/login/"
     ]
     if auth.require_auth(request.path, excluded_path):
-        if not auth.authorization_header(request):
+        if not auth.authorization_header(
+                request) and not auth.session_cookie(request):
             abort(401)
         if not auth.current_user(request):
             abort(403)

902c2014b2d66a166dc2ad98f2286f8152764ead
keme-ebi <kelvin6bolou@gmail.com>
2024-04-17 22:51:54 +0100

    added a method to SessionAuth that creates a session ID for a user_id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,8 +2,22 @@
 """SessionAuth
 """
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """class that inherits from Auth class"""
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """creates a session ID for a user_id
+        Arg:
+            user_id(str): the user id to create a session ID for
+        Return:
+            the session ID
+        """
+        if not user_id or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id

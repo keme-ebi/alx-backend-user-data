f48ec1c67a7b9c16704a695e803dc946c49c8bf1
keme-ebi <kelvin6bolou@gmail.com>
2024-04-18 23:26:28 +0100

    created a new authentication system, based on Session ID stored in database

---
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""session_db_auth"""
+from api.v1.auth.session_exp_auth import SessionExpAuth
+from datetime import datetime, timedelta
+from models.user_session import UserSession
+
+
+class SessionDBAuth(SessionExpAuth):
+    """session database class"""
+    def create_session(self, user_id=None):
+        """creates and stores new instance of UserSession"""
+        session_id = super().create_session(user_id)
+        if not session_id:
+            return None
+        dic = {"user_id": user_id, "session_id": session_id}
+        user = UserSession(**dic)
+        user.save()
+        return session_id
+
+    def user_id_for_session_id(self, session_id=None):
+        """returns the User ID by requesting UserSession in the database
+            on session_id
+        """
+        if not session_id:
+            return None
+        user = UserSession.search({"session_id": session_id})
+        if not user:
+            return None
+        return user[0].user_id
+
+    def destroy_session(self, request=None):
+        """destroys the UserSession based on the Session ID
+            from the request cookie
+        """
+        if request is None:
+            return False
+        session = self.session_cookie(request)
+        if not session:
+            return False
+        user = UserSession.search({"session_id": session_id})
+        if not user:
+            return False
+        user[0].remove()
+        return True

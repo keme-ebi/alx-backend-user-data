279212fb364ef1f6db8f138ab1e188f37f369d22
keme-ebi <kelvin6bolou@gmail.com>
2024-04-19 11:59:51 +0100

    adjusted code for expiration

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """session_db_auth"""
-from api.v1.auth.session_exp_auth import SessionExpAuth
+from .session_exp_auth import SessionExpAuth
 from datetime import datetime, timedelta
 from models.user_session import UserSession
 
@@ -23,9 +23,28 @@ class SessionDBAuth(SessionExpAuth):
         """
         if not session_id:
             return None
-        user = UserSession.search({"session_id": session_id})
-        if not user:
+        # get user id from UserSession based on session_id
+        try:
+            user = UserSession.search({"session_id": session_id})
+        except Exception:
             return None
+        # get session when a key is equal to session id
+        session = self.user_id_by_session_id.get(session_id)
+        if not session:
+            return None
+        """ if session_duration is equal or under 10, return the
+            user id from UserSession in the database
+        """
+        if self.session_duration <= 0:
+            return user[0].user_id
+
+        created_at = session.get('created_at')
+        if not created_at:
+            return None
+        time = created_at + timedelta(seconds=self.session_duration)
+        if time < datetime.now():
+            return None
+        # default
         return user[0].user_id
 
     def destroy_session(self, request=None):

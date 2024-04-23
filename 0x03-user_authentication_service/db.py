7ef9f7735988ffa7e61b61e4f3be29a597734d4c
keme-ebi <kelvin6bolou@gmail.com>
2024-04-23 18:00:52 +0100

    implemented a method that locates a user and updates the user attributes

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -59,3 +59,15 @@ class DB:
         if not user:
             raise NoResultFound
         return user
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """locates user by id and updates it from the kwargs
+        Args:
+            user_id(int): the user id to locate
+            kwargs: key-word argument
+        """
+        user = self.find_user_by(id=user_id)
+        for key, value in kwargs.items():
+            if not hasattr(user, key):
+                raise ValueError
+            setattr(user, key, value)

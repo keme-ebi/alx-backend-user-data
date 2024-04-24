f94a15d556a079a4593531ba2d60e2736f9aac9b
keme-ebi <kelvin6bolou@gmail.com>
2024-04-24 21:44:17 +0100

    added a condition for if NoResultFound is raised

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -118,8 +118,9 @@ class Auth:
             the generated token if user exists
             otherwise raise ValueError exception
         """
-        user = self._db.find_user_by(email=email)
-        if not user:
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
             raise ValueError
         token = _generate_uuid()
         self._db.update_user(user.id, reset_token=token)

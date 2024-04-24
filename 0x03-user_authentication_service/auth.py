f9d3f95bd756b07200c6be9b59a121dd514c2358
keme-ebi <kelvin6bolou@gmail.com>
2024-04-24 21:40:24 +0100

    implemented a Auth method that takes an email as argument, finds the user corresponding to the email, generates a UUID and update the user reset_token database field, and returns the token

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -109,3 +109,18 @@ class Auth:
             return None
         self._db.update_user(user.id, session_id=None)
         return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """updates user's reset_token database field
+        Arg:
+            email(str): email to search for corresponding user
+        Return:
+            the generated token if user exists
+            otherwise raise ValueError exception
+        """
+        user = self._db.find_user_by(email=email)
+        if not user:
+            raise ValueError
+        token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=token)
+        return token

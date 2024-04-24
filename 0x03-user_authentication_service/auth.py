1af76838b96a155e0b14b14c7fad7af5110befa9
keme-ebi <kelvin6bolou@gmail.com>
2024-04-24 20:48:34 +0100

    implemented a new Auth method that returns a corresponding User based on the session_id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -5,6 +5,7 @@ import uuid
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
+from typing import Union
 
 
 def _hash_password(password: str) -> bytes:
@@ -77,3 +78,18 @@ class Auth:
         session_id = _generate_uuid()
         self._db.update_user(user.id, session_id=session_id)
         return session_id
+
+    def get_user_from_session_id(self, session_id: str) Union[User, None]:
+        """retrieves the corresponding User
+        Arg:
+            session_id(str): the session_id to retrieve user from
+        Return:
+            corresponding user, otherwise None
+        """
+        if not session_id:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
+        return user
